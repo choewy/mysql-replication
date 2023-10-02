c481e0017570f114b05b4399aa57a714c4f356b2
choewy <choewy32@gmail.com>
2023-10-02 15:23:07 +0900

    feature/app guards

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
 
-import { DatabaseConfig } from '@core/configs';
+import { DatabaseConfig, JwtConfig } from '@core/configs';
 import { DatabaseType } from '@core/constants';
+import { JwtGuard, JwtGuardStrategy } from '@common/guards';
 import * as entities from '@common/entities';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+
+import { AuthModule } from '@module/auth';
 import { UserModule } from '@module/user';
 
 const entityTargets = Object.values(entities);
@@ -15,11 +20,14 @@ const entityTargets = Object.values(entities);
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    ThrottlerModule.forRoot([{ ttl: 10000, limit: 30 }]),
     TypeOrmModule.forRoot(new DatabaseConfig('MASTER').getTypeOrmModuleOptions(entityTargets)),
     TypeOrmModule.forRoot(new DatabaseConfig('SLAVE').getTypeOrmModuleOptions(entityTargets, DatabaseType.SLAVE)),
+    JwtModule.register(new JwtConfig('JWT').getJwtModuleOptions()),
+    AuthModule,
     UserModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ThrottlerGuard, JwtGuardStrategy, JwtGuard],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
+import { ThrottlerGuard } from '@nestjs/throttler';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  app.useGlobalGuards(app.get(ThrottlerGuard));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
   await app.listen(3000);
 }
 bootstrap();
